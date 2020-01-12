9e76277a1ab854f0e187873368b5af53c714bcb7
Laurent Bourgon <laurenttripleor@gmail.com>
2020-01-11 21:44:33 -0500

    added selecting days compatibility

--- run_bot.py
+++ run_bot.py
@@ -3,12 +3,15 @@ from os import environ
 
 if __name__ == "__main__":
     try:
-        from auth_keys import BOT_HTTP, CHANNEL_ID, ADMIN_ID, TIME_HOUR, TIME_MIN
+        from auth_keys import BOT_HTTP, CHANNEL_ID, ADMIN_ID, TIME_DAYS, TIME_HOUR, TIME_MIN
     except ImportError:
         BOT_HTTP = environ["SUT_BOT"]
         CHANNEL_ID = environ["SUT_CHANNEL"]
         ADMIN_ID = environ["SUT_ADMIN"]
+        TIME_DAYS = tuple(environ["SUT_TIME_DAYS"])
         TIME_HOUR = environ["SUT_TIME_HOUR"]
         TIME_MIN = environ["SUT_TIME_MIN"]
+        
 
-    forecast.Forecast(BOT_HTTP, CHANNEL_ID, ADMIN_ID, TIME_HOUR, TIME_MIN)
+    forecast.Forecast(BOT_HTTP, CHANNEL_ID, ADMIN_ID,
+                    TIME_DAYS, TIME_HOUR, TIME_MIN)
