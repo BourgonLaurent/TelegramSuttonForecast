214677568ea0f0328c53cff0224c7a3857c037f3
Laurent Bourgon <laurenttripleor@gmail.com>
2020-02-14 18:13:02 -0500

    Fix Message of the Day

--- suttonforecast/journalist.py
+++ suttonforecast/journalist.py
@@ -47,8 +47,8 @@ class Journalist:
         self.data["chalets"]["ouvert"], self.data["chalets"]["index"] = soup.find_all("div", attrs={"class":"icon_block"})[1].h3.text.strip().split("/")
         
         self.data["conditions"]["surface"], self.data["conditions"]["base"], self.data["conditions"]["couverture"] = [info.text.strip() for info in soup.find("div", attrs={"class":"surface"}).findAll("p")]
-        self.data["conditions"]["message"] = soup.find("section", attrs={"class":"sutton-comment"}).find("div", attrs={"class": "_5w1r _3_om _5wdf"}).text.strip().replace(u"\xa0","").replace("\n"," ")
-        print(self.data["conditions"]["message"])
+        self.data["conditions"]["message"] = soup.find("section", attrs={"class":"sutton-comment"}).text.strip().replace(u"\xa0","").replace("\n"," ")
+        # print(self.data["conditions"]["message"])
 
     def processStatut(self, soup):
         for i, table in enumerate(soup.find_all("table")):
